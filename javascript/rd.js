c378e9bb18560c73853939ab46f6a69701d098e3
Maaz <maazshaikh7711@gmail.com>
2022-01-14 08:24:44 +0530

    Completed Countdownt-timer

---
+++ javascript/rd.js
@@ -0,0 +1,22 @@
+const day = document.getElementById("days");
+const hour = document.getElementById("hours");
+const minute = document.getElementById("minutes");
+const second = document.getElementById("seconds");
+
+function countdown() {
+  const currentDate = new Date();
+  const newyearDate = new Date("2022-02-April");
+  const totalsecond = (newyearDate - currentDate) / 1000;
+  const days = Math.floor(totalsecond / 3600 / 24);
+  const hours = Math.floor(totalsecond / 3600) % 24;
+  const minutes = Math.floor(totalsecond / 60) % 60;
+  const seconds = Math.floor(totalsecond % 60);
+  // console.log(newyearDate-currentDate )
+  console.log(days, hours, minutes, seconds);
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  minute.innerHTML = minutes;
+  second.innerHTML = seconds;
+}
+countdown();
+setInterval(countdown, 1000);
